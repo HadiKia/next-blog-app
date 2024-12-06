e1ae80f1827f5dbdb51f13108071ec499b7ebc0d
HadiKia <hadikiamarzi@protonmail.com>
2024-12-07 00:04:29 +0330

    Fetching Data in Server Components

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+async function CategoryList() {
+  const res = await fetch("http://localhost:5000/api/category/list");
+  const {
+    data: { categories },
+  } = await res.json();
+
+  console.log(categories);
+
+  return (
+    <ul className="space-y-4">
+      <Link href={`/blogs/`}>همه</Link>
+      {categories.map((category) => {
+        return (
+          <li key={category._id}>
+            <Link href={`/blogs/category/${category.slug}`}>
+              {category.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default CategoryList;
