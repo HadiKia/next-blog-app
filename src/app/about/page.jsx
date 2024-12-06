c01bf4f451ddd2b9519bb4e2736b85dd882bb56b
HadiKia <hadikiamarzi@protonmail.com>
2024-12-06 21:22:33 +0330

    Adding Metadata

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  // title : "About",
+  title: {
+    absolute: "About", // ignore %s in parent layout.
+  },
+};
+
+function About() {
+  return <div>About</div>;
+}
+
+export default About;
