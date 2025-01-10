f445d48913e04531573bb68a704ee965933adfb3
HadiKia <hadikiamarzi@protonmail.com>
2025-01-10 19:47:45 +0330

    More on Next.js Image Component

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -9,8 +9,16 @@ async function PostList() {
   return posts.length > 0 ? (
     <div className="grid grid-cols-12 gap-8">
       {posts.map((post) => (
-        <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg">
-          <Image src={post.coverImageUrl} alt="" width={400} height={400} />
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded-lg">
+          <div className="relative aspect-video rounded-md overflow-hidden mb-6">
+            <Image
+              src={post.coverImageUrl}
+              alt={post.title}
+              fill
+              className="object-cover object-center hover:scale-110 transition-all duration-300 ease-linear"
+              quality={80}
+            />
+          </div>
         </div>
       ))}
     </div>
