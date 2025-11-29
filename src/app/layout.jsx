12fe11a2c1c9840dcd27b30f41a19b85503f8936
HadiKia <hadikiamarzi@protonmail.com>
2025-11-29 13:29:04 +0330

    Home page created

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import vazirFont from "@/constants/localFont";
+import "@/styles/globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl" className="light-mode">
+      <body className={`${vazirFont.variable} font-sans min-h-screen`}>
+        <Header />
+        <div className="container xl:max-w-screen-xl">{children}</div>
+      </body>
+    </html>
+  );
+}
