a845506a1a975933f89e22f40a23c550d25036ea
HadiKia <hadikiamarzi@protonmail.com>
2025-12-10 06:47:20 +0330

    feat: add Modal component

--- src/app/blogs/_components/comment/PostComments.jsx
+++ src/app/blogs/_components/comment/PostComments.jsx
@@ -4,14 +4,30 @@ import Button from "@/ui/Button";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import Comment from "./Comment";
 import classNames from "classnames";
+import Modal from "@/ui/Modal";
+import { useState } from "react";
 
 const PostComments = ({ post: { comments, _id: postId } }) => {
+  const [open, setIsOpen] = useState(false);
+
   return (
     <div className="my-20">
+      <Modal
+        open={open}
+        onClose={() => setIsOpen(false)}
+        title="عنوان مودال"
+        description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ"
+      >
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ،
+      </Modal>
       <div className="flex flex-col items-center justify-between gap-y-6 ">
         <div className="w-full flex items-center justify-between">
           <h2 className="text-2xl font-bold text-secondary-800">نظرات</h2>
-          <Button variant="outline" className="flex items-center gap-x-2 py-2">
+          <Button
+            variant="outline"
+            className="flex items-center gap-x-2 py-2"
+            onClick={() => setIsOpen(true)}
+          >
             <QuestionMarkCircleIcon className="w-5 h-5 mb-0.5" />
             <span>ثبت نظر جدید</span>
           </Button>
---
+++ src/ui/Modal.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children, title, description, open, onClose }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const ref = useOutsideClick(() => {
+    onClose?.();
+  });
+
+  useEffect(() => {
+    let rafId;
+    let timeoutId;
+
+    if (open) {
+      setIsMounted(true);
+      rafId = requestAnimationFrame(() => {
+        rafId = requestAnimationFrame(() => {
+          setIsVisible(true);
+        });
+      });
+    } else {
+      setIsVisible(false);
+      timeoutId = setTimeout(() => {
+        setIsMounted(false);
+      }, 300);
+    }
+
+    return () => {
+      if (rafId) cancelAnimationFrame(rafId);
+      if (timeoutId) clearTimeout(timeoutId);
+    };
+  }, [open]);
+
+  if (!isMounted) return null;
+
+  return createPortal(
+    <>
+      {/* backdrop */}
+      <div
+        aria-hidden
+        className={`fixed inset-0 bg-black/40 backdrop-blur-sm z-10  duration-300 ease-linear
+          ${isVisible ? "opacity-100" : "opacity-0"}
+        `}
+      />
+
+      {/* modal */}
+      <div
+        ref={ref}
+        role="dialog"
+        aria-modal="true"
+        className={`fixed z-20 inset-x-0 bottom-0 md:mx-auto md:bottom-auto md:top-1/2 md:-translate-y-1/2 md:max-w-xl bg-secondary-0 dark:bg-secondary-50 rounded-t-xl md:rounded-xl p-6 max-h-[calc(100%-100px)] overflow-auto scrollbar-thin scrollbar-thumb-primary-200 scrollbar-track-transparent scrollbar-thumb-rounded-xl duration-300 ease-linear
+          ${
+            isVisible
+              ? "translate-y-0 opacity-100"
+              : "translate-y-[100vh] opacity-0"
+          }
+        `}
+      >
+        {/* header */}
+        <div className="pb-4 border-b border-secondary-200 mb-8">
+          <div className="flex items-center justify-between text-secondary-700">
+            <h6 className="text-lg font-semibold">{title}</h6>
+            <button
+              onClick={onClose}
+              type="button"
+              className="p-2 -m-2"
+              aria-label="Close"
+            >
+              <XMarkIcon className="w-6 h-6" />
+            </button>
+          </div>
+
+          {description && (
+            <p className="text-primary-400 text-sm lg:text-base mt-4">
+              {description}
+            </p>
+          )}
+        </div>
+
+        <div>{children}</div>
+      </div>
+    </>,
+    document.body
+  );
+};
+
+export default Modal;
