34422bb75737ddf61393d92c84f393b6d752004d
HadiKia <hadikiamarzi@protonmail.com>
2025-12-12 02:07:09 +0330

    feat: improve Search component functionality

--- src/ui/Search.jsx
+++ src/ui/Search.jsx
@@ -1,24 +1,43 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 const Search = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
 
+  const currentSearch = searchParams.get("search") || "";
+  const [searchValue, setSearchValue] = useState(currentSearch);
+
+  useEffect(() => {
+    setSearchValue(currentSearch);
+  }, [currentSearch]);
+
   const formSubmit = (e) => {
     e.preventDefault();
-    const searchValue = e.target.search.value;
     const newParams = new URLSearchParams(searchParams.toString());
     if (searchValue) {
       newParams.set("search", searchValue);
     } else {
       newParams.delete("search");
     }
+    router.push(`${pathName}?${newParams.toString()}`, {
+      scroll: false,
+      shallow: true,
+    });
+  };
 
-    router.push(`${pathName}?${newParams.toString()}`, { scroll: false });
+  const clearSearch = () => {
+    setSearchValue("");
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.delete("search");
+    router.push(`${pathName}?${newParams.toString()}`, {
+      scroll: false,
+      shallow: true,
+    });
   };
 
   return (
@@ -32,10 +51,26 @@ const Search = () => {
         autoComplete="off"
         type="text"
         name="search"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
-      <button className="absolute end-[1px] top-[1px] bottom-[1px] pe-4 ps-2  rounded-e-lg ">
-        <MagnifyingGlassIcon className="w-5 h-5 text-secondary-400" />
-      </button>
+
+      {currentSearch ? (
+        <button
+          type="button"
+          className="absolute end-[1px] top-[1px] bottom-[1px] pe-4 ps-2 rounded-e-lg"
+          onClick={clearSearch}
+        >
+          <XMarkIcon className="w-5 h-5 text-secondary-400" />
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="absolute end-[1px] top-[1px] bottom-[1px] pe-4 ps-2 rounded-e-lg"
+        >
+          <MagnifyingGlassIcon className="w-5 h-5 text-secondary-400" />
+        </button>
+      )}
     </form>
   );
 };
