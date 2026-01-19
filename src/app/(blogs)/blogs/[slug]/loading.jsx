8a1fed32f88ceb91b1b4b390b4016b1b65a77256
HadiKia <hadikiamarzi@protonmail.com>
2026-01-19 17:43:46 +0330

    feat: add skeleton for blog details page

--- src/app/(blogs)/blogs/[slug]/loading.jsx
+++ src/app/(blogs)/blogs/[slug]/loading.jsx
@@ -1,11 +1,78 @@
-import Spinner from "@/ui/Spinner";
-import React from "react";
-
 const Loading = () => {
   return (
-    <div className="flex flex-col gap-y-10 items-center justify-center">
-      <span className="text-lg text-secondary-500">درحال بارگزاری اطلاعات</span>
-      <Spinner />
+    <div className="pb-20 lg:pt-10 grid lg:grid-cols-12 gap-8 lg:gap-y-10">
+      <div className="lg:col-span-8 lg:order-2 ">
+        <div className="relative flex flex-col lg:flex-col-reverse gap-y-6 mb-4 lg:mb-8">
+          <div className="flex flex-col gap-y-3 lg:gap-y-6 lg:flex-col-reverse">
+            <div className="relative aspect-video overflow-hidden lg:rounded-lg -mx-4 sm:mx-0 bg-secondary-200 animate-pulse"></div>
+
+            <div className="flex flex-col gap-3 lg:flex-row lg:items-center ">
+              <div className="flex items-center justify-start gap-x-2">
+                <div className="w-6 h-6 bg-secondary-200 animate-pulse rounded"></div>
+                <div className="bg-secondary-200 animate-pulse h-6 w-28 rounded"></div>
+              </div>
+
+              <span className="hidden lg:block w-px"></span>
+
+              <div className="flex items-center gap-2 lg:gap-3 w-full">
+                <div className="flex items-center gap-x-1 w-full lg:w-fit">
+                  <div className="w-4 h-4 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse"></div>
+                  <div className="bg-secondary-200 animate-pulse h-4 lg:h-6 w-full lg:w-20 rounded"></div>
+                </div>
+
+                <span className="w-px h-4 bg-secondary-300"></span>
+
+                <div className="flex items-center gap-x-1 w-full lg:w-fit">
+                  <div className="w-4 h-4 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse"></div>
+                  <div className="bg-secondary-200 animate-pulse h-4 lg:h-6 w-full lg:w-20 rounded"></div>
+                </div>
+
+                <span className="w-px h-4 bg-secondary-300"></span>
+
+                <div className="flex items-center gap-x-1 w-full lg:w-fit">
+                  <div className="w-4 h-4 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse"></div>
+                  <div className="bg-secondary-200 animate-pulse h-4 lg:h-6 w-full lg:w-20 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="h-8 lg:h-9 w-full lg:w-60 bg-secondary-200 animate-pulse rounded-md"></div>
+
+          <div className="z-[1] lg:z-0 fixed bottom-0 inset-x-0 px-4 py-2 lg:p-0 bg-secondary-0 border-t border-secondary-200 lg:bg-transparent lg:border-none lg:absolute  lg:bottom-auto lg:top-0 lg:start-auto w-full lg:w-fit flex items-start justify-evenly gap-3">
+            <span className="w-5 h-5 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse m-1"></span>
+            <span className="w-5 h-5 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse m-1"></span>
+            <span className="w-5 h-5 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse m-1"></span>
+            <span className="w-5 h-5 lg:w-6 lg:h-6 rounded bg-secondary-200 animate-pulse m-1"></span>
+          </div>
+        </div>
+
+        <div className="w-full h-96 lg:h-40 bg-secondary-200 animate-pulse rounded-md"></div>
+      </div>
+
+      <div className="lg:col-span-4 lg:order-3 lg:mt-[50px]">
+        <div className="w-40 h-8 bg-secondary-200 animate-pulse rounded mb-6 "></div>
+        <div className="flex flex-col">
+          {[...Array(3)].map((_, index) => (
+            <div
+              className="border-t last-of-type:border-b border-e border-secondary-200 bg-secondary-50 flex last-of-type:rounded-b-lg first-of-type:rounded-t-lg overflow-hidden"
+              key={index}
+            >
+              <div className="relative aspect-square w-full max-w-32 bg-secondary-200 animate-pulse"></div>
+              <div className="p-4 flex flex-col w-full">
+                <div className="w-full h-7 bg-secondary-200 animate-pulse rounded"></div>
+
+                <div className="mt-auto">
+                  <div className="flex items-center justify-start gap-x-2">
+                    <div className="w-6 h-6 bg-secondary-200 animate-pulse rounded"></div>
+                    <div className="bg-secondary-200 animate-pulse h-6 w-28 rounded"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
