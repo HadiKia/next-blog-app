6d817a553f5288fffad9370a7521bd6a3bf2b632
HadiKia <hadikiamarzi@protonmail.com>
2026-02-05 16:14:48 +0330

    fix: validate Author avatar URL

--- src/app/(blogs)/blogs/_components/Author.jsx
+++ src/app/(blogs)/blogs/_components/Author.jsx
@@ -1,9 +1,12 @@
 import Avatar from "@/ui/Avatar";
 
 const Author = ({ name, avatarUrl }) => {
+  const validAvatarUrl =
+    !avatarUrl || avatarUrl.endsWith("/null") ? undefined : avatarUrl;
+
   return (
     <div className="flex items-center justify-start gap-x-2">
-      <Avatar src={avatarUrl} width={24} />
+      <Avatar src={validAvatarUrl} width={24} />
       <span className="text-sm text-secondary-600">{name}</span>
     </div>
   );
