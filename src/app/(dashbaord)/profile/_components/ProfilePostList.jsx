9db81eaa334e71629e051feeb7dcd60aa2e7cb27
HadiKia <hadikiamarzi@protonmail.com>
2026-02-13 21:49:01 +0330

    feat: redesign blog cards in ProfilePostList

--- src/app/(dashbaord)/profile/_components/ProfilePostList.jsx
+++ src/app/(dashbaord)/profile/_components/ProfilePostList.jsx
@@ -6,14 +6,14 @@ import { getPostsByIds } from "@/services/postServices";
 import { toast } from "react-hot-toast";
 import { usePathname } from "next/navigation";
 import { toLocalDateShort2 } from "@/utils/dateFormatter";
-import { toPersianDigits } from "@/utils/numberFormatter";
-import { CalendarDateRangeIcon, ClockIcon } from "@heroicons/react/24/outline";
+import { CalendarDateRangeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import truncateText from "@/utils/truncateText";
 import Button from "@/ui/Button";
 import CoverImage from "app/(blogs)/blogs/_components/CoverImage";
 import ProfilePostListSkeleton from "./ProfilePostListSkeleton";
 import Empty from "@/ui/Empty";
+import Author from "app/(blogs)/blogs/_components/Author";
 
 const ProfilePostList = ({
   title,
@@ -78,14 +78,14 @@ const ProfilePostList = ({
         <h2 className="text-2xl font-bold text-secondary-700">{title}</h2>
       </div>
 
-      <div className="flex flex-col sm:grid grid-cols-12 md:gap-6 -mx-4 sm:mx-0 ">
+      <div className="flex flex-col sm:grid grid-cols-12 gap-6 lg:gap-8">
         {posts.map((post) => (
           <div
-            className="col-span-12 md:col-span-6 border-t last-of-type:border-b md:border border-secondary-200 bg-secondary-50 md:rounded-xl md:overflow-hidden flex "
+            className="col-span-12 md:col-span-6 xl:col-span-4 border border-secondary-200 bg-secondary-50 rounded-xl overflow-hidden flex flex-col"
             key={post._id}
           >
             <CoverImage {...post} />
-            <div className="p-4 flex flex-col w-full">
+            <div className="p-4 flex flex-col w-full h-full">
               <div className="flex items-start justify-between gap-x-2 mb-2 ">
                 <Link href={`/blogs/${post.slug}`}>
                   <h2 className=" font-bold text-lg text-secondary-700 hover:text-secondary-800 duration-300 ease-out">
@@ -101,23 +101,22 @@ const ProfilePostList = ({
                 </Button>
               </div>
 
-              <Link href={`/blogs/${post.slug}`}>
-                <p className="mb-6 text-secondary-400 text-sm hover:text-secondary-500 duration-300 ease-out">
-                  {truncateText(post.briefText, 50)}
-                </p>
-              </Link>
+              <div className="mt-auto">
+                <Link href={`/blogs/${post.slug}`}>
+                  <p className="text-secondary-400 text-sm hover:text-secondary-500 duration-300 ease-out mb-3">
+                    {truncateText(post.briefText, 80)}
+                  </p>
+                </Link>
 
-              <div className="flex items-center gap-x-3 mt-auto">
-                <div className="flex items-center gap-x-1 text-xs lg:text-sm text-secondary-500">
-                  <CalendarDateRangeIcon className="w-4 h-4 lg:w-5 lg:h-5 mb-0.5" />
-                  <span>{toLocalDateShort2(post.createdAt)}</span>
-                </div>
+                <div className="flex items-center gap-x-4 pt-3 border-t border-secondary-200">
+                  <Author {...post.author} />
 
-                <span className="w-px h-full bg-secondary-300"></span>
+                  <span className="blog h-5 w-px bg-secondary-200"></span>
 
-                <div className="flex items-center gap-x-1 text-xs lg:text-sm text-secondary-500">
-                  <ClockIcon className="w-4 h-4 lg:w-5 lg:h-5 mb-0.5" />
-                  <span>{toPersianDigits(post.readingTime)} دقیقه</span>
+                  <div className="flex items-center gap-x-1 text-xs lg:text-sm text-secondary-500">
+                    <CalendarDateRangeIcon className="w-4 h-4 lg:w-5 lg:h-5 mb-0.5" />
+                    <span>{toLocalDateShort2(post.createdAt)}</span>
+                  </div>
                 </div>
               </div>
             </div>
